2b88716451695409c7dd9b9d7a5feca66fd5d61f
Marcel Scheck <marcel.scheck@gmx.de>
2018-05-26 17:18:51 +0200

    solution for FizzBuzz

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+
+    private static final int MAX_VALUE = 100;
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new FizzBuzz().generate()));
+    }
+
+    String[] generate() {
+        return IntStream.rangeClosed(1, MAX_VALUE)
+        .mapToObj(this::replaceNumberWithFizzAndBuzz)
+        .toArray(String[]::new);
+    }
+
+    private String replaceNumberWithFizzAndBuzz(int value) {
+        String result = "";
+
+        if (isMultipleOfThree(value)) {
+            result += "Fizz";
+        }
+        if (isMultipleOfFive(value)) {
+            result += "Buzz";
+        }
+        if (result.isEmpty()) {
+            result += String.valueOf(value);
+        }
+        return result;
+    }
+
+    private boolean isMultipleOfThree(int value) {
+        return value % 3 == 0;
+    }
+
+    private boolean isMultipleOfFive(int value) {
+        return value % 5 == 0;
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void init () {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void testLength() {
+        String[] actual = fizzBuzz.generate();
+        Assertions.assertEquals(100, actual.length);
+    }
+
+    @Test
+    void testFirstItem() {
+        String[] actual = fizzBuzz.generate();
+        Assertions.assertEquals("1", actual[0]);
+    }
+
+    @Test
+    void testFirstFizz() {
+        String[] actual = fizzBuzz.generate();
+        Assertions.assertEquals("Fizz", actual[2]);
+    }
+
+    @Test
+    void testFirstBuzz() {
+        String[] actual = fizzBuzz.generate();
+        Assertions.assertEquals("Buzz", actual[4]);
+    }
+
+    @Test
+    void testFirstFizzBuzz() {
+        String[] actual = fizzBuzz.generate();
+        Assertions.assertEquals("FizzBuzz", actual[14]);
+    }
+}
