af324c2e79f967d57173133875d9df207ba7db6b
Marcel Scheck <marcel.scheck@gmx.de>
2018-07-02 14:23:18 +0200

    Hangman Kata works

--- src/main/java/classes/Hangman.java
+++ src/main/java/classes/Hangman.java
@@ -1,4 +1,48 @@
 package classes;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Hangman {
+
+    private static final char CHARACTER_PLACEHOLDER = '-';
+
+    private char[] searchedWord;
+    private boolean[] guessMask;
+
+    public Hangman(String searchedWord) {
+        validateWord(searchedWord);
+        this.guessMask = new boolean[searchedWord.length()];
+        Arrays.fill(guessMask, false);
+        this.searchedWord = searchedWord.toCharArray();
+    }
+
+    private void validateWord(String word) {
+        if (word.isEmpty()) throw new IllegalArgumentException("Word must not be empty");
+        if (!word.matches("([a-z]|[A-Z])*")) throw new IllegalArgumentException("Use only letters");
+    }
+
+
+    String guess(char letter) {
+        findOccurrences(letter);
+        return getGuessedWord();
+    }
+
+    private void findOccurrences(char letter) {
+        IntStream.range(0, searchedWord.length)
+                .forEach(i -> {
+                    if (searchedWord[i] == letter) guessMask[i] = true;
+                });
+    }
+
+
+    private String getGuessedWord() {
+        char[] result = new char[searchedWord.length];
+        IntStream.range(0, searchedWord.length)
+                .forEach(i -> {
+                    if (guessMask[i]) result[i] = searchedWord[i];
+                    else result[i] = CHARACTER_PLACEHOLDER;
+                });
+        return String.valueOf(result);
+    }
 }
--- src/test/java/classes/HangmanTest.java
+++ src/test/java/classes/HangmanTest.java
@@ -1,4 +1,64 @@
 package classes;
 
-public class HangmanTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+class HangmanTest {
+
+    @Test
+    void testExceptionForEmptyString() {
+        Executable executable = () -> new Hangman("");
+        Assertions.assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    void testExceptionForNumbers() {
+        Executable executable = () -> new Hangman("1");
+        Assertions.assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    void testExceptionForSpecialCharacters() {
+        Executable executable = () -> new Hangman("$");
+        Assertions.assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    void testNoExceptionForLowerCaseLetters() {
+        new Hangman("a");
+    }
+
+    @Test
+    void testNoExceptionForUpperCaseLetters() {
+        new Hangman("A");
+    }
+
+    @Test
+    void testNoExceptionForWord() {
+        new Hangman("HelloWorld");
+    }
+
+    @Test
+    void testOneLetterFail() {
+        Hangman hangman = new Hangman("A");
+        String actual = hangman.guess('a');
+        Assertions.assertEquals("-", actual);
+    }
+
+    @Test
+    void testOneLetterSuccess() {
+        Hangman hangman = new Hangman("A");
+        String actual = hangman.guess('A');
+        Assertions.assertEquals("A", actual);
+    }
+
+    @Test
+    void testTwoLetterSuccess() {
+        Hangman hangman = new Hangman("is");
+        String actual = hangman.guess('i');
+        Assertions.assertEquals("i-", actual);
+        actual = hangman.guess('s');
+        Assertions.assertEquals("is", actual);
+    }
 }
