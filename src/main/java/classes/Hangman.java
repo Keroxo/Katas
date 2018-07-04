d86d47042927c259e7304410a23c0c39115169bc
Marcel Scheck <marcel.scheck@gmx.de>
2018-07-04 22:36:15 +0200

    Hangman Kata refactored

--- src/main/java/classes/Hangman.java
+++ src/main/java/classes/Hangman.java
@@ -6,26 +6,30 @@ import java.util.stream.IntStream;
 public class Hangman {
 
     private static final char CHARACTER_PLACEHOLDER = '-';
+    private static final String REGEX_LETTER = "([a-z]|[A-Z])*";
 
     private char[] searchedWord;
     private boolean[] guessMask;
 
     public Hangman(String searchedWord) {
         validateWord(searchedWord);
-        this.guessMask = new boolean[searchedWord.length()];
-        Arrays.fill(guessMask, false);
+        initGuessMask(searchedWord);
         this.searchedWord = searchedWord.toCharArray();
     }
 
     private void validateWord(String word) {
         if (word.isEmpty()) throw new IllegalArgumentException("Word must not be empty");
-        if (!word.matches("([a-z]|[A-Z])*")) throw new IllegalArgumentException("Use only letters");
+        if (!word.matches(REGEX_LETTER)) throw new IllegalArgumentException("Use only letters");
     }
 
+    private void initGuessMask(String searchedWord) {
+        this.guessMask = new boolean[searchedWord.length()];
+        Arrays.fill(guessMask, false);
+    }
 
     String guess(char letter) {
         findOccurrences(letter);
-        return getGuessedWord();
+        return calculateGuessedWord();
     }
 
     private void findOccurrences(char letter) {
@@ -35,14 +39,11 @@ public class Hangman {
                 });
     }
 
-
-    private String getGuessedWord() {
+    private String calculateGuessedWord() {
         char[] result = new char[searchedWord.length];
+
         IntStream.range(0, searchedWord.length)
-                .forEach(i -> {
-                    if (guessMask[i]) result[i] = searchedWord[i];
-                    else result[i] = CHARACTER_PLACEHOLDER;
-                });
+                .forEach(i -> result[i] = guessMask[i] ? searchedWord[i] : CHARACTER_PLACEHOLDER);
         return String.valueOf(result);
     }
 }
